9776eccae301c6224e2557ee53c5033febe7b51c
Casieee <cen-le@sjtu.edu.cn>
2021-07-19 17:39:30 +0800

    change logo

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import * as React from 'react';
 import { NavigationContainer} from '@react-navigation/native';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import {CardStyleInterpolators, createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CreateMeetingScreen from "./pages/CreateMeetingScreen";
 import JoinMeetingScreen from "./pages/JoinMeetingScreen";
-import HomeScreen from "./pages/HomeScreen";
-import UserScreen from "./pages/UserScreen";
 import HistoryScreen from "./pages/HistoryScreen";
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {config} from "./utils/Constants";
 import {TextButton} from "./components/MyButton";
 import ReServeMeetingScreen from "./pages/ReServeMeetingScreen";
 import LoginScreen from "./pages/Login";
@@ -18,33 +13,7 @@ import RegisterScreen from "./pages/RegisterScreen";
 import MeetingPage from "./pages/Meeting";
 import MeetingSettingScreen from "./pages/MeetingSetting";
 import UserSettingScreen from "./pages/UserSetting";
-
-const Tab = createBottomTabNavigator();
-
-function TabScreen() {
-    return (
-        <Tab.Navigator
-            screenOptions={({route}) => ({
-                tabBarIcon: ({focused}) => {
-                    if (route.name === 'Home') {
-                        return focused ? <Ionicons name="time" color={config.qGreen} size={30}/> :
-                            <Ionicons name={"time-outline"} size={25}/>;
-                    } else if (route.name === 'User') {
-                        return focused ? <Ionicons name="person" color={config.qGreen} size={30}/> :
-                            <Ionicons name={"person-outline"} size={25}/>;
-                    }
-                },
-            })}
-            tabBarOptions={{
-                activeTintColor: config.qGreen,
-                inactiveTintColor: 'gray',
-            }}
-        >
-            <Tab.Screen name={"Home"} component={HomeScreen}/>
-            <Tab.Screen name={"User"} component={UserScreen}/>
-        </Tab.Navigator>
-    )
-}
+import TabScreen from "./pages/TabRouter";
 
 const forFade = ({ current }) => ({
     cardStyle: {
