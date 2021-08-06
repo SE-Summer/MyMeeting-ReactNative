688323287eb45fcb0ede2c2f9bfb96f8a01859d0
Naplesoul <shenwhang@outlook.com>
2021-08-06 17:32:58 +0800

    Improve: better speech recognition

--- src/utils/media/SpeechRecognition.ts
+++ src/utils/media/SpeechRecognition.ts
@@ -2,6 +2,7 @@ import { Recognizer } from "react-native-speech-iflytek";
 import {NativeEventEmitter} from "react-native";
 import {iflytekAPPID} from "../../ServiceConfig";
 import {SpeechText} from "../Types";
+import * as moment from "moment";
 
 Recognizer.init(iflytekAPPID);
 
@@ -10,18 +11,18 @@ export class SpeechRecognition
     private recognizerEventEmitter = new NativeEventEmitter(Recognizer);
     private working: boolean = null;
     private sentenceEnded: boolean = null;
-    private recognizedCallbacks: Map<string, (result) => void> = null;
+    private recognizedCallbacks: Map<string, (result: SpeechText) => void> = null;
 
     constructor()
     {
-        this.recognizedCallbacks = new Map<string, (result) => void>();
+        this.recognizedCallbacks = new Map<string, (result: SpeechText) => void>();
         this.recognizerEventEmitter.addListener('onRecognizerResult', this.onRecognizerResult);
         this.recognizerEventEmitter.addListener('onRecognizerError', (err) => {console.error(err)});
         this.working = false;
         this.sentenceEnded = true;
     }
 
-    public registerRecognizedListener(key: string, recognizedCallback: (result) => void)
+    public registerRecognizedListener(key: string, recognizedCallback: (text: SpeechText) => void)
     {
         this.recognizedCallbacks.set(key, recognizedCallback);
     }
@@ -32,16 +33,38 @@ export class SpeechRecognition
     }
 
     private onRecognizerResult = (result) => {
-        if (result.isLast && this.working) {
-            Recognizer.start();
-            console.log('[Recognizer]  Sentence ended')
+        const text = result.text.trim();
+        if (result.isLast) {
+            console.log(`[Recognizer]  Recognized speech: ${text}`);
+            console.log('[Recognizer]  Sentence ended');
+            let speechText: SpeechText = {
+                fromMyself: true,
+                sentenceEnded: false,
+                text: text,
+                timestamp: this.sentenceEnded ? moment() : null,
+            }
+            this.sentenceEnded = true;
+            this.recognizedCallbacks.forEach((callback) => {
+                callback(speechText);
+            });
+            if (this.working) {
+                Recognizer.start();
+            }
+        } else if (text == '') {
+            return;
+        } else {
+            console.log(`[Recognizer]  Recognized speech: ${text}`);
+            let speechText: SpeechText = {
+                fromMyself: true,
+                sentenceEnded: false,
+                text: text,
+                timestamp: this.sentenceEnded ? moment() : null,
+            }
+            this.recognizedCallbacks.forEach((callback) => {
+                callback(speechText);
+            });
+            this.sentenceEnded = false;
         }
-
-        console.log('[Recognizer]  Word recognized: ' + result.text);
-
-        this.recognizedCallbacks.forEach((callback) => {
-            callback(result);
-        });
     }
 
     private sendSpeechText(text: SpeechText)
